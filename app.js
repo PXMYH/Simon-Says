ce42b18517685f9e4eff40703051d0382a4dabcd
Michael Hu <pxmyhdev@gmail.com>
2019-03-22 20:14:50 -0400

    add view

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const path = require("path");
 const routes = require("./routes/index");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+// render html views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// parse content
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/", routes);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+var tcom = require("thesaurus-com");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("It works!");
+  res.render("form", { title: "Registration form" });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.render("form", { title: "Registration form" });
 });
 
 module.exports = router;
