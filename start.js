00c7028641703396fca1885e8e3395580674efa8
Michael Hu <pxmyhdev@gmail.com>
2019-03-23 01:36:25 -0400

    try to resolve Heroku R10 boot timeout issue

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
+const port = process.env.PORT || 3000;
+const host = "0.0.0.0";
+const server = app.listen(port, host, function() {
   console.log(`Express is running on port ${server.address().port}`);
 });
